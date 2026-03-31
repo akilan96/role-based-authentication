30414ce73683764ddb79f144a5b92ae4c2f31956
Akilan <mailtoakilan04gmail.com>
2026-03-31 18:47:39 +0530

    db env variable fix

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose"
 const connectDB = async () =>{
 
     console.log("👉 Connecting to MongoDB...");
-    console.log("👉 URI:", process.env.MONGO_URI);
-    
     await mongoose.connect(process.env.DB_URL)
     .then(()=>console.log("DB Connected for role-auth"))
     .catch((e) => console.log(e.message))
